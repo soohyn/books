f0598ed743e17ee15737e280361599dbc45fd250
soohyn <sooheyonjo@gmail.com>
2026-02-06 15:32:48 +0900

    feat: header nav item 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,21 @@
+import { Link } from "react-router";
+
 const Header = () => {
-  return <header>header component</header>;
+  return (
+    <header>
+      <h1>CERTICOS BOOKS</h1>
+      <nav aria-label="메인 메뉴">
+        <ul>
+          <li>
+            <Link to="/search">도서 검색</Link>
+          </li>
+          <li>
+            <Link to="/like">내가 찜한 책</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
