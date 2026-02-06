1e0fa7ac7d72799d7955adc9fb1fe03fc8bbb373
soohyn <sooheyonjo@gmail.com>
2026-02-06 11:55:39 +0900

    add: 리액트 라우터, 리액트 쿼리 라이브러리 설치 및 기본 설정

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/index.css";
 import App from "./App.tsx";
+import { BrowserRouter } from "react-router";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/queryClient";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </BrowserRouter>
   </StrictMode>,
 );
