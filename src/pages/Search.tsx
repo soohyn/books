35de813ae6e172a9917f565aa433373ca0cce20c
soohyn <sooheyonjo@gmail.com>
2026-02-07 15:03:51 +0900

    feat: 검색 기록 스토리지 저장 로직 작성

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BookListItem from "../components/BookListItem";
 import SearchBox from "../components/SearchBox";
 import SearchCountText from "../components/SearchCountText";
@@ -6,18 +6,59 @@ import { useQuery } from "@tanstack/react-query";
 import { requestBooks } from "../api/book";
 import NoData from "../components/NoData";
 
+const SEARCH_HISTORY_KEY = "search-history";
+
+const getStorageHistory = () => {
+  const storageData: string = localStorage.getItem(SEARCH_HISTORY_KEY) ?? "";
+
+  return JSON.parse(storageData);
+};
+
 function Search() {
   const [inputValue, setInputValue] = useState("");
   const [searchQuery, setSearchQuery] = useState<string>("");
+  const [searchHistory, setSearchHistory] =
+    useState<string[]>(getStorageHistory);
 
   const { data } = useQuery({
     queryKey: ["books", searchQuery],
     queryFn: () => requestBooks({ query: searchQuery }),
   });
 
+  const setStorageHistory = (searchHistory: string[]) => {
+    localStorage.setItem(SEARCH_HISTORY_KEY, JSON.stringify(searchHistory));
+  };
+
+  const addSearchHistory = (searchQuery: string) => {
+    setSearchHistory((prev) => {
+      const newHistory = prev.filter((item) => item !== searchQuery);
+
+      if (newHistory.length >= 8) {
+        return [...newHistory.slice(0, 1), searchQuery];
+      } else {
+        return [...newHistory, searchQuery];
+      }
+    });
+  };
+
+  const deleteSearchHistory = (deleteItem: string) => {
+    setSearchHistory((prev) => prev.filter((item) => item !== deleteItem));
+  };
+
   const handleOnSearch = () => {
     setSearchQuery(inputValue);
+    addSearchHistory(inputValue);
   };
+
+  const handleClickHistory = (history: string) => {
+    setInputValue(history);
+    setSearchQuery(history);
+  };
+
+  useEffect(() => {
+    setStorageHistory(searchHistory);
+  }, [searchHistory]);
+
   return (
     <main>
       <section>
@@ -27,11 +68,26 @@ function Search() {
           setInputValue={setInputValue}
           onSearch={handleOnSearch}
         />
+        {searchHistory.reverse().map((item, index) => {
+          return (
+            <div key={item + index}>
+              <span onClick={() => handleClickHistory(item)}>{item}</span>
+              <button
+                onClick={() => {
+                  deleteSearchHistory(item);
+                }}
+              >
+                x
+              </button>
+            </div>
+          );
+        })}
         <SearchCountText count={data?.meta.total_count ?? 0} />
         {(data?.meta.total_count ?? 0 > 0) ? (
-          data?.documents.map((item) => {
+          data?.documents.map((item, index) => {
             return (
               <BookListItem
+                key={item.title + index}
                 title={item.title}
                 author={item.authors.join(", ")}
                 price={item.sale_price}
